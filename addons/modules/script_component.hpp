868c089558729a0a99653a6d70052c02310a1eba
3Mydlo3 <mydlo333@interia.pl>
2019-12-03 18:44:03 +0100

    Create modules component

---
+++ addons/modules/script_component.hpp
@@ -0,0 +1,14 @@
+#define COMPONENT modules
+#include "\z\afsk\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_MODULES
+    #define DEBUG_MODE_FULL
+#endif
+    #ifdef DEBUG_SETTINGS_MODULES
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_MODULES
+#endif
+
+#include "\z\afsk\addons\main\script_macros.hpp"
