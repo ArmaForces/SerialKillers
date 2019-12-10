ced86cb8080d8c616f2ee99a2e90efa9447b9c35
3Mydlo3 <mydlo333@interia.pl>
2019-12-10 20:53:10 +0100

    Create score component

---
+++ addons/score/script_component.hpp
@@ -0,0 +1,14 @@
+#define COMPONENT score
+#include "\z\afsk\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_SCORE
+    #define DEBUG_MODE_FULL
+#endif
+    #ifdef DEBUG_SETTINGS_SCORE
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_SCORE
+#endif
+
+#include "\z\afsk\addons\main\script_macros.hpp"
