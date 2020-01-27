2dd6aa55fa2d8c853e85cc8d36f5bd353fc7c6e2
3Mydlo3 <mydlo333@interia.pl>
2020-01-27 21:34:59 +0100

    Create `civilian_statemachine` component

---
+++ addons/civilian_statemachine/config.cpp
@@ -0,0 +1,18 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "afsk_civilian"
+        };
+        author = "ArmaForces";
+        VERSION_CONFIG;
+    };
+};
+
+
+#include "CfgEventHandlers.hpp"
