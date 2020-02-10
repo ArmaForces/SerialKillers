932635abcb5a0ac4644bb9e25e33dd42eb78e7ad
3Mydlo3 <mydlo333@interia.pl>
2020-02-10 14:15:20 +0100

    Create medical component

---
+++ addons/medical/script_component.hpp
@@ -0,0 +1,14 @@
+#define COMPONENT medical
+#include "\z\afsk\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_MEDICAL
+    #define DEBUG_MODE_FULL
+#endif
+    #ifdef DEBUG_SETTINGS_MEDICAL
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_MEDICAL
+#endif
+
+#include "\z\afsk\addons\main\script_macros.hpp"
