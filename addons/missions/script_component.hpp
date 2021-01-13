822ca90e8f1c0a7c99e2a3875b74cd3cef64bcb7
3Mydlo3 <mydlo333@interia.pl>
2021-01-13 20:19:05 +0100

    Add initial version of mission on Malden

---
+++ addons/missions/script_component.hpp
@@ -0,0 +1,19 @@
+#define COMPONENT missions
+#include "\z\afsk\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_MISSIONS
+    #define DEBUG_MODE_FULL
+#endif
+    #ifdef DEBUG_SETTINGS_MISSIONS
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_MISSIONS
+#endif
+
+#define MISSION_CONFIG(MISSION_NAME,MAP) \
+        class MISSION_NAME { \
+            directory = QUOTE(PATHTO_R(MISSION_NAME.MAP)); \
+        }
+
+#include "\z\afsk\addons\main\script_macros.hpp"
