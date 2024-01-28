2c0a4360fb7dac8d9fa53f1f792c472fd9ae1dd0
3Mydlo3 <mydlo333@interia.pl>
2024-01-28 18:57:30 +0100

    Replace & with _

--- addons/missions/CfgMissions.hpp
+++ addons/missions/CfgMissions.hpp
@@ -1,6 +1,6 @@
 class CfgMissions {
     class MPMissions {
-        MISSION_CONFIG(SerialKillers_CUP&RHS,Chernarus_Winter);
+        MISSION_CONFIG(SerialKillers_CUP_RHS,Chernarus_Winter);
         MISSION_CONFIG(SK_test,Malden);
     };
 };
