609f5f5574bd5097ce5e515795f001111a24e139
3Mydlo3 <mydlo333@interia.pl>
2024-03-05 19:54:06 +0100

    Add Lower Berezino location to Chernarus map

---
+++ addons/cup_maps/CfgWorlds.hpp
@@ -0,0 +1,55 @@
+class CfgWorlds
+{
+    class CAWorld;
+
+    class Chernarus : CAWorld
+    {
+        class Names
+        {
+            class vill_LowerBerezino
+            {
+                angle = 0;
+                name = CSTRING(LowerBerezino);
+                position[] = { 12920, 9990 };
+                radiusA = 200;
+                radiusB = 200;
+                speech[] = { "Berezino" };
+                type = "NameCity";
+            };
+        };
+    };
+
+    class Chernarus_Summer : CAWorld
+    {
+        class Names
+        {
+            class vill_LowerBerezino
+            {
+                angle = 0;
+                name = CSTRING(LowerBerezino);
+                position[] = { 12920, 9990 };
+                radiusA = 200;
+                radiusB = 200;
+                speech[] = { "Berezino" };
+                type = "NameCity";
+            };
+        };
+    };
+
+    class Chernarus_Winter : CAWorld
+    {
+        class Names
+        {
+            class vill_LowerBerezino
+            {
+                angle = 0;
+                name = CSTRING(LowerBerezino);
+                position[] = { 12920, 9990 };
+                radiusA = 200;
+                radiusB = 200;
+                speech[] = { "Berezino" };
+                type = "NameCity";
+            };
+        };
+    };
+};
