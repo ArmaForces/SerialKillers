6dcf092a618d24d2ca69fae4ecd701d982339907
3Mydlo3 <mydlo333@interia.pl>
2024-03-06 18:38:10 +0100

    Change size of some built-in areas

--- addons/cup_maps/CfgWorlds.hpp
+++ addons/cup_maps/CfgWorlds.hpp
@@ -6,24 +6,147 @@ class CfgWorlds
     {
         class Names
         {
+            class city_Berezino
+            {
+                radiusA = 700;
+                radiusB = 700;
+            };
+
+            class city_Chernogorsk
+            {
+                radiusA = 800;
+                radiusB = 800;
+            };
+
+            class city_Elektrozavodsk
+            {
+                angle = -20;
+                radiusA = 800;
+                radiusB = 600;
+            };
+
+            class city_Gorka {};
+
+            class city_Krasnostav
+            {
+                radiusA = 600;
+                radiusB = 600;
+            };
+
+            class city_Solnychniy {};
+            class city_StarySobor {};
+            class city_Vybor {};
+
+            class city_Zelenogorsk
+            {
+                angle = 45;
+                radiusA = 400;
+                radiusB = 600;
+            };
+
+            class vill_Balota {};
+            class vill_Bor {};
+            class vill_Dolina {};
+            class vill_Drozhino {};
+
+            class vill_Dubrovka
+            {
+                radiusA = 400;
+                radiusB = 400;
+            };
+
+            class vill_Grishino {};
+            class vill_Guglovo {};
+
+            class vill_Gvozdno
+            {
+                radiusA = 700;
+                radiusB = 700;
+            };
+
+            class vill_Kabanino {};
+            class vill_Kamenka {};
+            class vill_Kamyshovo {};
+
+            class vill_Khelm
+            {
+                radiusA = 150;
+                radiusB = 300;
+            };
+
+            class vill_Komarovo
+            {
+                radiusA = 200;
+            };
+
+            class vill_Kozlovka
+            {
+                radiusA = 200;
+                radiusB = 200;
+            };
+
+            class vill_Lopatino
+            {
+                radiusA = 200;
+            };
+
+            class vill_Mogilevka
+            {
+                radiusA = 300;
+                radiusB = 200;
+            };
+
+            class vill_Msta
+            {
+                angle = -30;
+                radiusA = 200;
+                radiusB = 150;
+            };
+
+            class vill_Myshkino {};
+
+            class vill_Nadezhdino
+            {
+                radiusB = 300;
+            };
+
+            class vill_Nizhnoye {};
+            class vill_NovySobor {};
+            class vill_Olsha {};
+            class vill_Orlovets {};
+            class vill_Pavlovo {};
+            class vill_Petrovka {};
+            class vill_Pogorevka {};
+            class vill_Polana {};
+            class vill_Prigorodki {};
+            class vill_Pulkovo {};
+            class vill_Pusta {};
+            class vill_Pustoshka {};
+            class vill_Rogovo {};
+            class vill_Shakhovka {};
+            class vill_Sosnovka {};
+            class vill_Staroye {};
+            class vill_Tulga {};
+            class vill_Vyshnoye {};
+
             class vill_NorthernBalota
             {
                 angle = 0;
                 name = CSTRING(NorthernBalota);
                 position[] = { 4880, 2830 };
-                radiusA = 100;
-                radiusB = 100;
+                radiusA = 50;
+                radiusB = 50;
                 speech[] = { "Balota" };
                 type = "NameVillage";
             };
 
             class vill_LowerBerezino
             {
-                angle = 0;
+                angle = 15;
                 name = CSTRING(LowerBerezino);
                 position[] = { 12920, 9990 };
-                radiusA = 200;
-                radiusB = 200;
+                radiusA = 400;
+                radiusB = 600;
                 speech[] = { "Berezino" };
                 type = "NameVillage";
             };
@@ -34,24 +157,147 @@ class CfgWorlds
     {
         class Names
         {
+            class city_Berezino
+            {
+                radiusA = 700;
+                radiusB = 700;
+            };
+
+            class city_Chernogorsk
+            {
+                radiusA = 800;
+                radiusB = 800;
+            };
+
+            class city_Elektrozavodsk
+            {
+                angle = -20;
+                radiusA = 800;
+                radiusB = 600;
+            };
+
+            class city_Gorka {};
+
+            class city_Krasnostav
+            {
+                radiusA = 600;
+                radiusB = 600;
+            };
+
+            class city_Solnychniy {};
+            class city_StarySobor {};
+            class city_Vybor {};
+
+            class city_Zelenogorsk
+            {
+                angle = 45;
+                radiusA = 400;
+                radiusB = 600;
+            };
+
+            class vill_Balota {};
+            class vill_Bor {};
+            class vill_Dolina {};
+            class vill_Drozhino {};
+
+            class vill_Dubrovka
+            {
+                radiusA = 400;
+                radiusB = 400;
+            };
+
+            class vill_Grishino {};
+            class vill_Guglovo {};
+
+            class vill_Gvozdno
+            {
+                radiusA = 700;
+                radiusB = 700;
+            };
+
+            class vill_Kabanino {};
+            class vill_Kamenka {};
+            class vill_Kamyshovo {};
+
+            class vill_Khelm
+            {
+                radiusA = 150;
+                radiusB = 300;
+            };
+
+            class vill_Komarovo
+            {
+                radiusA = 200;
+            };
+
+            class vill_Kozlovka
+            {
+                radiusA = 200;
+                radiusB = 200;
+            };
+
+            class vill_Lopatino
+            {
+                radiusA = 200;
+            };
+
+            class vill_Mogilevka
+            {
+                radiusA = 300;
+                radiusB = 200;
+            };
+
+            class vill_Msta
+            {
+                angle = -30;
+                radiusA = 200;
+                radiusB = 150;
+            };
+
+            class vill_Myshkino {};
+
+            class vill_Nadezhdino
+            {
+                radiusB = 300;
+            };
+
+            class vill_Nizhnoye {};
+            class vill_NovySobor {};
+            class vill_Olsha {};
+            class vill_Orlovets {};
+            class vill_Pavlovo {};
+            class vill_Petrovka {};
+            class vill_Pogorevka {};
+            class vill_Polana {};
+            class vill_Prigorodki {};
+            class vill_Pulkovo {};
+            class vill_Pusta {};
+            class vill_Pustoshka {};
+            class vill_Rogovo {};
+            class vill_Shakhovka {};
+            class vill_Sosnovka {};
+            class vill_Staroye {};
+            class vill_Tulga {};
+            class vill_Vyshnoye {};
+
             class vill_NorthernBalota
             {
                 angle = 0;
                 name = CSTRING(NorthernBalota);
                 position[] = { 4880, 2830 };
-                radiusA = 100;
-                radiusB = 100;
+                radiusA = 50;
+                radiusB = 50;
                 speech[] = { "Balota" };
                 type = "NameVillage";
             };
 
             class vill_LowerBerezino
             {
-                angle = 0;
+                angle = 15;
                 name = CSTRING(LowerBerezino);
                 position[] = { 12920, 9990 };
-                radiusA = 200;
-                radiusB = 200;
+                radiusA = 400;
+                radiusB = 600;
                 speech[] = { "Berezino" };
                 type = "NameVillage";
             };
@@ -62,24 +308,147 @@ class CfgWorlds
     {
         class Names
         {
+            class city_Berezino
+            {
+                radiusA = 700;
+                radiusB = 700;
+            };
+
+            class city_Chernogorsk
+            {
+                radiusA = 800;
+                radiusB = 800;
+            };
+
+            class city_Elektrozavodsk
+            {
+                angle = -20;
+                radiusA = 800;
+                radiusB = 600;
+            };
+
+            class city_Gorka {};
+
+            class city_Krasnostav
+            {
+                radiusA = 600;
+                radiusB = 600;
+            };
+
+            class city_Solnychniy {};
+            class city_StarySobor {};
+            class city_Vybor {};
+
+            class city_Zelenogorsk
+            {
+                angle = 45;
+                radiusA = 400;
+                radiusB = 600;
+            };
+
+            class vill_Balota {};
+            class vill_Bor {};
+            class vill_Dolina {};
+            class vill_Drozhino {};
+
+            class vill_Dubrovka
+            {
+                radiusA = 400;
+                radiusB = 400;
+            };
+
+            class vill_Grishino {};
+            class vill_Guglovo {};
+
+            class vill_Gvozdno
+            {
+                radiusA = 700;
+                radiusB = 700;
+            };
+
+            class vill_Kabanino {};
+            class vill_Kamenka {};
+            class vill_Kamyshovo {};
+
+            class vill_Khelm
+            {
+                radiusA = 150;
+                radiusB = 300;
+            };
+
+            class vill_Komarovo
+            {
+                radiusA = 200;
+            };
+
+            class vill_Kozlovka
+            {
+                radiusA = 200;
+                radiusB = 200;
+            };
+
+            class vill_Lopatino
+            {
+                radiusA = 200;
+            };
+
+            class vill_Mogilevka
+            {
+                radiusA = 300;
+                radiusB = 200;
+            };
+
+            class vill_Msta
+            {
+                angle = -30;
+                radiusA = 200;
+                radiusB = 150;
+            };
+
+            class vill_Myshkino {};
+
+            class vill_Nadezhdino
+            {
+                radiusB = 300;
+            };
+
+            class vill_Nizhnoye {};
+            class vill_NovySobor {};
+            class vill_Olsha {};
+            class vill_Orlovets {};
+            class vill_Pavlovo {};
+            class vill_Petrovka {};
+            class vill_Pogorevka {};
+            class vill_Polana {};
+            class vill_Prigorodki {};
+            class vill_Pulkovo {};
+            class vill_Pusta {};
+            class vill_Pustoshka {};
+            class vill_Rogovo {};
+            class vill_Shakhovka {};
+            class vill_Sosnovka {};
+            class vill_Staroye {};
+            class vill_Tulga {};
+            class vill_Vyshnoye {};
+
             class vill_NorthernBalota
             {
                 angle = 0;
                 name = CSTRING(NorthernBalota);
                 position[] = { 4880, 2830 };
-                radiusA = 100;
-                radiusB = 100;
+                radiusA = 50;
+                radiusB = 50;
                 speech[] = { "Balota" };
                 type = "NameVillage";
             };
 
             class vill_LowerBerezino
             {
-                angle = 0;
+                angle = 15;
                 name = CSTRING(LowerBerezino);
                 position[] = { 12920, 9990 };
-                radiusA = 200;
-                radiusB = 200;
+                radiusA = 400;
+                radiusB = 600;
                 speech[] = { "Berezino" };
                 type = "NameVillage";
             };
