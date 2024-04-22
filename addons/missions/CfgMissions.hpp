d6802ffb64d42cc99cfd4f93b5b92f49664fee6a
3Mydlo3 <mydlo333@interia.pl>
2024-04-22 18:51:34 +0200

    Rename folder

--- addons/missions/CfgMissions.hpp
+++ addons/missions/CfgMissions.hpp
@@ -1,9 +1,9 @@
 class CfgMissions {
     class MPMissions {
+        MISSION_CONFIG(SerialKillers,Woodland_ACR);
         MISSION_CONFIG(SerialKillers_CUP_RHS,Chernarus);
         MISSION_CONFIG(SerialKillers_CUP_RHS,Chernarus_Winter);
         MISSION_CONFIG(SerialKillers_CUP_RHS,Chernarus_Summer);
-        MISSION_CONFIG(SerialKillers_CUP_RHS,Woodland_ACR);
         MISSION_CONFIG(SK_test,Malden);
     };
 };
