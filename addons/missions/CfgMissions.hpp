f75f314469cfb851e600b8ab552429291e536937
3Mydlo3 <mydlo333@interia.pl>
2025-02-28 13:52:15 +0100

    Reorder MISSION_CONFIG

--- addons/missions/CfgMissions.hpp
+++ addons/missions/CfgMissions.hpp
@@ -1,10 +1,10 @@
 class CfgMissions {
     class MPMissions {
-        MISSION_CONFIG(SerialKillers,Woodland_ACR);
         MISSION_CONFIG(SerialKillers_CUP_RHS,Chernarus);
         MISSION_CONFIG(SerialKillers_CUP_RHS,Chernarus_Winter);
         MISSION_CONFIG(SerialKillers_CUP_RHS,Chernarus_Summer);
         MISSION_CONFIG(SerialKillers,Tanoa);
+        MISSION_CONFIG(SerialKillers,Woodland_ACR); // Bystrica
         MISSION_CONFIG(SK_test,Malden);
     };
 };
